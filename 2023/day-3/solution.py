30ceddca2c55b1e5dea7c1d6010610709631dd99
Justin Dennison <justin1dennison@gmail.com>
2023-12-03 01:27:05 -0500

    chore: part 2 of day 3 complete

--- 2023/day-3/solution.py
+++ 2023/day-3/solution.py
@@ -23,7 +23,9 @@ class Number:
     end: int
 
     def is_near(self, symbol: Symbol) -> bool:
-        return (self.row - symbol.row in {-1, 0, 1}) and (self.start - 1 <= symbol.position <= self.end)
+        return (self.row - symbol.row in {-1, 0, 1}) and (
+            self.start - 1 <= symbol.position <= self.end
+        )
 
 
 def one(data: str) -> int:
@@ -46,8 +48,37 @@ def one(data: str) -> int:
     return sum(n.value for n in numbers for s in symbols if n.is_near(s))
 
 
+def two(data: str) -> int:
+    numpattern = re.compile(r"(?P<number>\d+)")
+    symbolpattern = re.compile(r"(?P<symbol>[*])")
+    numbers = []
+    symbols = []
+    for i, line in enumerate(str.splitlines(data)):
+        num_matches = (
+            (m.groupdict()["number"], m.span()) for m in numpattern.finditer(line)
+        )
+        for value, (start, end) in num_matches:
+            numbers.append(Number(row=i, value=int(value), start=start, end=end))
+
+        symbol_matches = (
+            (m.groupdict()["symbol"], m.span()) for m in symbolpattern.finditer(line)
+        )
+        for value, (position, _) in symbol_matches:
+            symbols.append(Symbol(row=i, value=value, position=position))
+    total = 0
+    for symbol in symbols:
+        ns = [n for n in numbers if n.is_near(symbol)]
+        if len(ns) == 2:
+            total += ns[0].value * ns[1].value
+    return total
+
+
 if __name__ == "__main__":
     data = read_data("./input.txt")
-    one_sample = one(read_data("./sample1.txt"))
+    sample_data = read_data("./sample1.txt")
+    one_sample = one(sample_data)
     assert one_sample == 4361, f"{one_sample=}"
     print(f"{one(data)=}")
+    two_sample = two(sample_data)
+    assert two_sample == 467835, f"{two_sample=}"
+    print(f"{two(data)=}")
