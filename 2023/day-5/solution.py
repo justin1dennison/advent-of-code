b1229adef3e52979fb98a81f342cccafc0331f4b
Justin Dennison <justin1dennison@gmail.com>
2023-12-05 13:59:12 -0500

    chore(wip): day 5 part 2

--- 2023/day-5/solution.py
+++ 2023/day-5/solution.py
@@ -1,5 +1,7 @@
 import re
 from dataclasses import dataclass
+from functools import partial
+from itertools import islice, tee
 
 
 def read_data(filepath: str) -> str:
@@ -18,6 +20,12 @@ class MapEntry:
 
     def translate(self, value: int) -> int:
         return self.destination + abs(self.source - value)
+
+    def inverse_translate(self, value: int) -> int:
+        return self.source + abs(self.destination - value)
+
+    def in_destinations(self, value):
+        return self.destination <= value <= self.destination + self.length
 
 
 @dataclass
@@ -40,6 +48,47 @@ class Map:
             if value in entry:
                 return entry.translate(value)
         return value
+
+    def inverse(self, value: int) -> int:
+        for entry in self.entries:
+            if entry.in_destinations(value):
+                return entry.inverse_translate(value)
+        return value
+
+    def destinations(self):
+        for e in self.entries:
+            for n in range(e.destination, e.destination + e.length):
+                yield n
+
+
+@dataclass
+class SeedBank:
+    ranges: list[range]
+
+    @classmethod
+    def from_section(cls, section: str) -> "SeedBank":
+        one, two = tee((int(s) for s in section[len("seeds:") :].split(" ") if s), 2)
+        starts = islice(one, 0, None, 2)
+        lengths = islice(two, 1, None, 2)
+        seed_ranges = [range(s, s + l) for s, l in zip(starts, lengths)]
+        return cls(ranges=seed_ranges)
+
+    def __iter__(self):
+        for seed_range in self.ranges:
+            for seed in seed_range:
+                yield seed
+
+    def __len__(self):
+        return sum(len(r) for r in self.ranges)
+
+    def __contains__(self, value):
+        return any(value in r for r in self.ranges)
+
+
+def apply(pipeline: list[Map], value: int) -> int:
+    for fn in pipeline:
+        value = fn(value)
+    return value
 
 
 def one(data: str) -> int:
@@ -71,13 +120,42 @@ def one(data: str) -> int:
         temperature_to_humidity_map,
         humidity_to_location_map,
     ]
+    fn = partial(apply, pipeline)
+    return min(fn(seed) for seed in seeds)
 
-    locations = []
-    for seed in seeds:
-        for fn in pipeline:
-            seed = fn(seed)
-        locations.append(seed)
-    return min(locations)
+
+def two(data: str) -> int:
+    sections = data.split("\n\n")
+    (
+        seeds_data,
+        seed_to_soil_map_data,
+        soil_to_fertilizer_map_data,
+        fertilizer_to_water_map_data,
+        water_to_light_map_data,
+        light_to_temperature_map_data,
+        temperature_to_humidity_map_data,
+        humidity_to_location_map_data,
+    ) = sections
+
+    seeds = SeedBank.from_section(seeds_data)
+    seed_to_soil_map = Map.from_section(seed_to_soil_map_data)
+    soil_to_fertilizer_map = Map.from_section(soil_to_fertilizer_map_data)
+    fertilizer_to_water_map = Map.from_section(fertilizer_to_water_map_data)
+    water_to_light_map = Map.from_section(water_to_light_map_data)
+    light_to_temperature_map = Map.from_section(light_to_temperature_map_data)
+    temperature_to_humidity_map = Map.from_section(temperature_to_humidity_map_data)
+    humidity_to_location_map = Map.from_section(humidity_to_location_map_data)
+    pipeline = [
+        seed_to_soil_map,
+        soil_to_fertilizer_map,
+        fertilizer_to_water_map,
+        water_to_light_map,
+        light_to_temperature_map,
+        temperature_to_humidity_map,
+        humidity_to_location_map,
+    ]
+
+    fn = partial(apply, reversed(pipeline))
 
 
 if __name__ == "__main__":
@@ -85,6 +163,5 @@ if __name__ == "__main__":
     sample_data = read_data("./sample.txt")
     assert one(sample_data) == 35, f"{one(sample_data)=}"
     print(f"{one(data)=}")
-
-    assert two(sample_data) == 0, f"{two(sample_data)=}"
-    print(f"{two(data)=}")
+    assert two(sample_data) == 46, f"{two(sample_data)=}"
+    # print(f"{two(data)=}")
