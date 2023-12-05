3fc7abcc2818a5ffffd73dad5521f01ad417d72c
Justin Dennison <justin1dennison@gmail.com>
2023-12-04 22:18:24 -0500

    refactor: day-4

--- 2023/day-4/solution.py
+++ 2023/day-4/solution.py
@@ -1,7 +1,7 @@
-import re
 import math
-from typing import Callable, Iterable
+import re
 from dataclasses import dataclass, field
+from typing import Callable
 
 
 def read_data(filepath: str) -> str:
@@ -9,58 +9,56 @@ def read_data(filepath: str) -> str:
         return f.read()
 
 
-def parse_card(line: str) -> tuple[set[int], set[int]]:
-    pattern = re.compile(r"\d+")
-    card, numbers = line.split(":")
-    winning_raw, your_raw = numbers.split("|")
-    return set(int(n.group()) for n in pattern.finditer(winning_raw)), set(
-        int(n.group()) for n in pattern.finditer(your_raw)
-    )
-
-
-def score(card : str, score_fn: Callable[[Iterable[str]], int]) -> int:
-    winning, your = parse_card(card)
-    results = winning.intersection(your)
-    return score_fn(results)
-
-
-def one(data: str) -> int:
-    total = 0
-    for line in str.splitlines(data):
-        total += score(line, lambda r: math.floor(2 ** (len(r) - 1)))
-    return total
-
 @dataclass
 class Card:
     id: int
     winning: set[int]
     your: set[int]
-    count: int = field(default=1) 
+    count: int = field(default=1)
 
     @classmethod
-    def from_line(cls, id: int, line: str) -> "Card":
-        winning, your = parse_card(line)
-        return cls(id=id, winning=winning, your=your)
+    def from_line(cls, line: str) -> "Card":
+        pattern = re.compile(r"\d+")
+        id_pattern = re.compile(r"Card\s+(?P<id>\d+)")
+        card, numbers = line.split(":")
+        card_id = int(id_pattern.search(card).groupdict()["id"])
+        winning_raw, your_raw = numbers.split("|")
+        winning, your = set(int(n.group()) for n in pattern.finditer(winning_raw)), set(
+            int(n.group()) for n in pattern.finditer(your_raw)
+        )
+        return cls(id=card_id, winning=winning, your=your)
 
-    def score(self, score_fn: Callable[[Iterable[str]], int] = len) -> int:
+    def score(self, score_fn: Callable[[list[str]], int]) -> int:
         return score_fn(self.winning.intersection(self.your))
 
 
+def part_one_score_strategy(r: list[str]) -> int:
+    return math.floor(2 ** (len(r) - 1))
+
+
+def one(data: str) -> int:
+    cards = [Card.from_line(line) for line in str.splitlines(data)]
+    return sum(c.score(part_one_score_strategy) for c in cards)
+
+
+def part_two_score_strategy(r: list[str]) -> int:
+    return len(r)
+
+
 def two(data: str) -> int:
-    cards = [Card.from_line(i, line) for i, line in enumerate(str.splitlines(data))]
+    cards = [Card.from_line(line) for line in str.splitlines(data)]
     for card in cards:
-        score = card.score()
-        rewards = cards[card.id + 1: card.id + score + 1]
+        score = card.score(part_two_score_strategy)
+        rewards = cards[card.id : card.id + score]
         for reward in rewards:
             reward.count += card.count
     return sum(c.count for c in cards)
-    
-             
+
 
 if __name__ == "__main__":
     data = read_data("./input.txt")
     sample_data = read_data("./sample.txt")
-    assert one(sample_data) == 13
+    assert one(sample_data) == 13, f"{one(sample_data)=}"
     print(f"{one(data)=}")
     assert two(sample_data) == 30, f"{two(sample_data)=}"
     print(f"{two(data)=}")
