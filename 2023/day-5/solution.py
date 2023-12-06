0cf1d4207621be9222f5bbb9cfc7adbdaa59da98
Justin Dennison <justin1dennison@gmail.com>
2023-12-06 09:08:19 -0500

    chore: day 5 part 2 is a bust

--- 2023/day-5/solution.py
+++ 2023/day-5/solution.py
@@ -21,12 +21,6 @@ class MapEntry:
     def translate(self, value: int) -> int:
         return self.destination + abs(self.source - value)
 
-    def inverse_translate(self, value: int) -> int:
-        return self.source + abs(self.destination - value)
-
-    def in_destinations(self, value):
-        return self.destination <= value <= self.destination + self.length
-
 
 @dataclass
 class Map:
@@ -48,41 +42,6 @@ class Map:
             if value in entry:
                 return entry.translate(value)
         return value
-
-    def inverse(self, value: int) -> int:
-        for entry in self.entries:
-            if entry.in_destinations(value):
-                return entry.inverse_translate(value)
-        return value
-
-    def destinations(self):
-        for e in self.entries:
-            for n in range(e.destination, e.destination + e.length):
-                yield n
-
-
-@dataclass
-class SeedBank:
-    ranges: list[range]
-
-    @classmethod
-    def from_section(cls, section: str) -> "SeedBank":
-        one, two = tee((int(s) for s in section[len("seeds:") :].split(" ") if s), 2)
-        starts = islice(one, 0, None, 2)
-        lengths = islice(two, 1, None, 2)
-        seed_ranges = [range(s, s + l) for s, l in zip(starts, lengths)]
-        return cls(ranges=seed_ranges)
-
-    def __iter__(self):
-        for seed_range in self.ranges:
-            for seed in seed_range:
-                yield seed
-
-    def __len__(self):
-        return sum(len(r) for r in self.ranges)
-
-    def __contains__(self, value):
-        return any(value in r for r in self.ranges)
 
 
 def apply(pipeline: list[Map], value: int) -> int:
@@ -137,7 +96,14 @@ def two(data: str) -> int:
         humidity_to_location_map_data,
     ) = sections
 
-    seeds = SeedBank.from_section(seeds_data)
+    seeds_data = [int(s) for s in seeds_data[len("seeds:") :].split(" ") if s]
+    starts = seeds_data[::2]
+    lengths = seeds_data[1::2]
+    seeds = (
+        s
+        for start, length in zip(starts, lengths)
+        for s in range(start, start + length)
+    )
     seed_to_soil_map = Map.from_section(seed_to_soil_map_data)
     soil_to_fertilizer_map = Map.from_section(soil_to_fertilizer_map_data)
     fertilizer_to_water_map = Map.from_section(fertilizer_to_water_map_data)
@@ -154,8 +120,8 @@ def two(data: str) -> int:
         temperature_to_humidity_map,
         humidity_to_location_map,
     ]
-
-    fn = partial(apply, reversed(pipeline))
+    fn = partial(apply, pipeline)
+    return min(fn(s) for s in seeds)
 
 
 if __name__ == "__main__":
@@ -164,4 +130,4 @@ if __name__ == "__main__":
     assert one(sample_data) == 35, f"{one(sample_data)=}"
     print(f"{one(data)=}")
     assert two(sample_data) == 46, f"{two(sample_data)=}"
-    # print(f"{two(data)=}")
+    print(f"{two(data)=}")
