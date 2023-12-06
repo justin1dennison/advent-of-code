11b8ea7eda8a712ad9a4800243cb669130439cb0
Justin Dennison <justin1dennison@gmail.com>
2023-12-06 18:41:25 -0500

    chore: complete day 6 part 2

--- 2023/day-6/solution.py
+++ 2023/day-6/solution.py
@@ -1,5 +1,6 @@
 import re
 from functools import reduce
+from operator import mul
 
 
 def read_data(filepath: str) -> str:
@@ -20,11 +21,28 @@ def one(data: str) -> int:
         )
         for time, distance in zip(times, distances)
     ]
-    return reduce(lambda x, y: x * y, counts)
+    return reduce(mul, counts)
 
 
 def two(data: str) -> int:
-    ...
+    pattern = re.compile(r"\d+")
+    lines = [l for l in str.splitlines(data) if l]
+    times, distances = lines
+    time = int(''.join(f.group() for f in pattern.finditer(times)))
+    distance = int(''.join(f.group() for f in pattern.finditer(distances)))
+    times = range(time)
+
+    first = None
+    for t in times:
+        if t * (time - t) >= distance:
+            first = t
+            break
+    last = None
+    for t in reversed(times):
+        if t * (time - t) >= distance:
+            last = t
+            break
+    return last - first + 1
 
 
 if __name__ == "__main__":
@@ -32,5 +50,5 @@ if __name__ == "__main__":
     sample_data = read_data("./sample.txt")
     assert one(sample_data) == 288, f"{one(sample_data)=}"
     print(f"{one(data)=}")
-    assert two(sample_data) == 0, f"{two(sample_data)=}"
+    assert two(sample_data) == 71503, f"{two(sample_data)=}"
     print(f"{two(data)=}")
