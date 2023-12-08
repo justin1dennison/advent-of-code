470a26d4143edc91c3d8a696ddbd3c819643c725
Justin Dennison <justin1dennison@gmail.com>
2023-12-08 10:11:24 -0500

    chore(wip): incomplete day 7 part 2

--- 2023/day-7/solution.py
+++ 2023/day-7/solution.py
@@ -26,11 +26,12 @@ class Play(Enum):
 class Hand:
     cards: list[str]
     bid: int
+    values: str
 
     @classmethod
     def from_line(cls, line: str) -> "Hand":
         cards, bid = line.split(" ")
-        return cls(cards=list(cards), bid=int(bid))
+        return cls(cards=list(cards), bid=int(bid), values="23456789TJQKA")
 
     def play(self) -> Play:
         counts = Counter(self.cards)
@@ -51,11 +52,48 @@ class Hand:
         return Play.HIGH_CARD
 
     def __lt__(self, other: "Hand") -> bool:
-        card_ranks = {card: value for value, card in enumerate("23456789TJQKA", start=1)}
+        card_ranks = {
+            card: value for value, card in enumerate(self.values, start=1)
+        }
         for left, right in zip(self.cards, other.cards):
             if left != right:
-                return (self.play(), card_ranks[left]) < (other.play(), card_ranks[right])
+                return (self.play(), card_ranks[left]) < (
+                    other.play(),
+                    card_ranks[right],
+                )
 
+
+class HandWithJokers(Hand):
+    def __lt__(self, other: "HandWithJokers") -> bool:
+        card_ranks = {
+            card: value for value, card in enumerate("J23456789TQKA", start=1)
+        }
+        for left, right in zip(self.cards, other.cards):
+            if left != right:
+                return (self.play(), card_ranks[left]) < (
+                    other.play(),
+                    card_ranks[right],
+                )
+            
+    def play(self) -> Play:
+        counts = Counter(self.cards)
+        groups = counts.most_common(n=2)
+        if len(groups) == 1:
+            return Play.FIVE_OF_A_KIND
+
+        jokers = counts["J"]
+        (_, n), (_, k), *_ = groups
+        if (n + jokers) == 4:
+            return Play.FOUR_OF_A_KIND
+        if (n + jokers) == 3 and k == 2:
+            return Play.FULL_HOUSE
+        if (n + jokers) == 3 and k == 1:
+            return Play.THREE_OF_A_KIND
+        if (n + jokers) == 2 and k == 2:
+            return Play.TWO_PAIR
+        if (n + jokers) == 2 and k == 1:
+            return Play.ONE_PAIR
+        return Play.HIGH_CARD
 
 
 def one(data: str) -> int:
@@ -66,7 +104,10 @@ def one(data: str) -> int:
 
 
 def two(data: str) -> int:
-    ...
+    lines = [l for l in str.splitlines(data) if l]
+    hands = [HandWithJokers.from_line(line) for line in lines]
+    ranks = [(rank, hand) for rank, hand in enumerate(sorted(hands), start=1)]
+    return sum(rank * hand.bid for rank, hand in ranks)
 
 
 if __name__ == "__main__":
@@ -74,5 +115,5 @@ if __name__ == "__main__":
     sample_data = read_data("./sample.txt")
     assert one(sample_data) == 6440, f"{one(sample_data)=}"
     print(f"{one(data)=}")
-    assert two(sample_data) == 0, f"{two(sample_data)=}"
-    print(f"{two(data)=}")
+    assert two(sample_data) == 5905, f"{two(sample_data)=}"
+    #print(f"{two(data)=}")
